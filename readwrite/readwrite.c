d8bd708bcd7c86d7fb9859c3f405e9e7f0ecc31e
sudhakirang <sudhakirangunda@gmail.com>
2025-07-15 10:07:09 +0000

    new

--- readwrite/readwrite.c
+++ readwrite/readwrite.c
@@ -1,10 +1,24 @@
+// #include<stdio.h>
+// #define ADDRESS 0x40004000
+// int main()
+// {
+//     int write_val = 0x5A5A5A5A;
+//     int val_read;
+//     volatile int *ptr = (int *)ADDRESS;
+//     *ptr = write_val;
+//     val_read = *ptr;
+//     return 0;
+// }
+ 
+/*********or***********/
 #include<stdio.h>
 #define ADDRESS 0x40004000
+#define REG *((int *)ADDRESS)
 int main()
 {
     int write_val = 0x5A5A5A5A;
     int val_read;
-    *((int *)ADDRESS) = write_val;
-    val_read = *((int *)ADDRESS);
+    REG = write_val;
+    val_read = REG;
     return 0;
 }
