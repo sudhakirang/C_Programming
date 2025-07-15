3c5d3c157af364f7eb049197666dd283bce97844
Sudhakiran <sudhakirangunda@gmail.com>
2025-07-15 12:34:02 +0530

    Update nthbitclear.c

--- BIT_Operations/nthbitclear.c
+++ BIT_Operations/nthbitclear.c
@@ -1,1 +1,12 @@
-
+/*** pos value between 0 to 31*****/
+#include<stdio.h>
+int main()
+{
+  int a = 0x12345678;
+  int pos;    //position info to clear a bit
+  printf("enter the position where clear operation has to apply:\n");
+  scanf("%d",&pos);
+ a &=~(0x1<<pos) ;
+printf("a = %x\n",a);
+return 0;
+}
