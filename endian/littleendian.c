c214389a3eaf0a70cb01e0c78479bf200566f023
Sudhakiran <sudhakirangunda@gmail.com>
2025-07-15 11:53:33 +0530

    Update littleendian.c

--- endian/littleendian.c
+++ endian/littleendian.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+int main()
+{
+  int a = 0x12345678;
+  char *p = (char *)&a;
+  if(*p == 0x78)
+  {
+    printf("Little Endian\n");
+  }
+  else
+  {
+    printf("Big Endian\n");
+  }
+  
+  return 0;
+}
