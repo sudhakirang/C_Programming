ec91f50288a35d8b4fd4302f84974241497b49d6
Sudhakiran <sudhakirangunda@gmail.com>
2025-07-15 12:01:01 +0530

    Update LBendian.c

--- endian/LBendian.c
+++ endian/LBendian.c
@@ -1,1 +1,8 @@
-
+#include<stdio.h>
+int main()
+{
+  int a = 0x12345678;
+ a = (((a << 24)& 0xFF000000)|((a << 8)& 0x00FF0000)|((a >> 8)& 0x0000FF00)|((a >> 24)& 0x000000FF));
+printf("a = %x\n",a);
+return 0;
+}
