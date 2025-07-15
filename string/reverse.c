c779fd13608f3f8caab3ec731be46f6d0be8d19a
Sudhakiran <sudhakirangunda@gmail.com>
2025-07-15 11:01:58 +0530

    Update reverse.c

--- string/reverse.c
+++ string/reverse.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+int main()
+{
+  char a[20],temp;
+  int i,len=0;
+  printf("enter the string\n");
+  scanf("%[^\n]",a);
+  for(i=0;a[i]!='\0';i++)
+    {
+      len++;
+    }
+  printf("the length of the string is %d\n",len);
+  for(i=0;i<len/2;i++)
+    {
+      temp = a[i];
+      a[i] = a[len -i-1];
+      a[len - i -1] = temp;
+    }
+  printf("the string after reverse is %s\n",a);
+  return 0;
+}
