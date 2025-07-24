b5549333e593c400a7b715682e3dc02fcc9d85c3
Sudhakiran <sudhakirangunda@gmail.com>
2025-07-24 10:57:29 +0530

    Update eledel.c

--- array/eledel.c
+++ array/eledel.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int pos,i;
+    int size = sizeof(arr)/sizeof(arr[0]);          
+    printf("enter the position where the element is deleted\n");
+    scanf("%d",&pos);
+    for(i=pos;i<size-1;i++){
+          arr[i] = arr[i+1];
+    }
+    for(i=0;i<size-1;i++){
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+}
