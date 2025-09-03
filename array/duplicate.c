ee7dede66cf7b63cbdeab7555e2de995c70d515c
Sudhakiran <sudhakirangunda@gmail.com>
2025-09-03 15:09:47 +0530

    Update duplicate.c

--- array/duplicate.c
+++ array/duplicate.c
@@ -48,3 +48,33 @@ int main() {
 
     return 0;
 }
+
+
+
+
+/** another program **/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter how many elements in array a and b\n");
+    scanf("%d",&n);
+    int i,j,a[n];
+    int count = 0;
+    printf("enter the array A elements:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("the duplicate elements are:\n");
+    for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i] == a[j]){
+                printf("%d\t",a[i]);
+                count++;
+            }
+        }
+    }
+    if(!count){
+        printf("there are no common elements in the given array\n");
+    }
+    return 0;
+}
