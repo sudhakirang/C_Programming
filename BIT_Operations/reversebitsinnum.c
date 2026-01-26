88e2799e0a6091c811ef3306b0c4b56af5ffc3ea
Sudhakiran <sudhakirangunda@gmail.com>
2026-01-26 15:48:36 +0530

    Clarify comment on bit manipulation in reversebitsinnum.c
    Fixed comment formatting for clarity.

--- BIT_Operations/reversebitsinnum.c
+++ BIT_Operations/reversebitsinnum.c
@@ -5,7 +5,7 @@ int main(){
     scanf("%d",&num);
     int reverse_num=0;
     for(int i = 0; i<32;i++){
-        reverse_num = reverse_num<<1 | num&1;
+        reverse_num = reverse_num<<1 | num&1;      //num&1 can give a right most bit 
         num = num>>1;
     }
     printf("number after bit reverse is %d\n",reverse_num);
