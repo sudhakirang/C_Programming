3de756eea8c22f73eac704dec04d4f7d816753a2
Sudhakiran <sudhakirangunda@gmail.com>
2026-01-26 15:46:46 +0530

    Implement bit reversal for a given number

--- BIT_Operations/reversebitsinnum.c
+++ BIT_Operations/reversebitsinnum.c
@@ -1,1 +1,13 @@
-
+#include<stdio.h>
+int main(){
+    int num;
+    printf("enter the number\n");
+    scanf("%d",&num);
+    int reverse_num=0;
+    for(int i = 0; i<32;i++){
+        reverse_num = reverse_num<<1 | num&1;
+        num = num>>1;
+    }
+    printf("number after bit reverse is %d\n",reverse_num);
+    return 0;
+}
