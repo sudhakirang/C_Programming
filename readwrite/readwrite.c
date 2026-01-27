70c3e30d068f25727bb8a087b2da893b23f1c6f2
Sudhakiran <sudhakirangunda@gmail.com>
2026-01-27 16:52:12 +0530

    Update register definition for better type safety

--- readwrite/readwrite.c
+++ readwrite/readwrite.c
@@ -11,9 +11,22 @@
 // }
  
 /*********or***********/
+// #include<stdio.h>
+// #define ADDRESS 0x40004000
+// #define REG *((int *)ADDRESS)
+// int main()
+// {
+//     int write_val = 0x5A5A5A5A;
+//     int val_read;
+//     REG = write_val;
+//     val_read = REG;
+//     return 0;
+// }
+
+/************or************/
 #include<stdio.h>
-#define ADDRESS 0x40004000
-#define REG *((int *)ADDRESS)
+#include<stdint.h>
+#define REG (*(volatile uint32_t *)0x40004000)
 int main()
 {
     int write_val = 0x5A5A5A5A;
