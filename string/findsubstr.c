583b9eaa2bd813ec87038f355c2844507406f6bc
Sudhakiran <sudhakirangunda@gmail.com>
2026-02-05 15:35:05 +0530

    Add substring search functionality
    Implement substring search in a given string.

--- string/findsubstr.c
+++ string/findsubstr.c
@@ -1,1 +1,23 @@
-
+/**str[ ]=”aabbceaccabcdaebbcc” in this string find the Sub string abcd?****/
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[] = "aabbcabcdaebbcc”;
+    char sub_str[] = "abcd";
+    int i,j,m,n;
+    n = strlen(str);
+    m = strlen(sub_str);
+    for(i=0;i<n-m;i++){
+        for(j=0;j<m;j++){
+            if(str[i+j]!=sub_str[j]){
+                break;
+            }
+        }
+        if(j==m){
+                 printf("the sub string found at index %d\n",i);
+                 return 0;
+            }
+    }
+    printf("subtring not found\n");
+    return 0;
+}
