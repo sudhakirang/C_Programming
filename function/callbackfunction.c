dafecdf270d07a1657aeb27cd2f775cf418722d4
Sudhakiran <sudhakirangunda@gmail.com>
2026-02-06 17:06:42 +0530

    Add callback function example in C
    Implement a simple callback function example in C.

--- function/callbackfunction.c
+++ function/callbackfunction.c
@@ -1,1 +1,15 @@
+/***CALL BACK FUNCTION simple program
+Defination: A function that pass as an argument in another function and call/invoke in that function is called as call back function********/
+#include<stdio.h>
+void print_hello(void){
+  printf("Hello world\n");
+}
+void execute_call_back_function(void (*callback)()){
+  printf("executing the call back function\n");
+  callback();
+}
+void main(){
+  execute_call_back_function(print_hello);
+}
+
 
