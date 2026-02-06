e8b075efa4fd251e429105a22a08b9c5c4e41ded
Sudhakiran <sudhakirangunda@gmail.com>
2026-02-06 20:02:40 +0530

    Implement callback function with addition and multiplication
    Added callback function implementation with addition and multiplication examples.

--- function/callbackfunction.c
+++ function/callbackfunction.c
@@ -12,4 +12,25 @@ void main(){
   execute_call_back_function(print_hello);
 }
 
-
+Application on call back function (where function pointer also included):
+#include<stdio.h>
+int addition(int c, int d){
+  return c+d;
+}
+int multiply(int a,int b){
+    return a*b;
+}
+int execute_call_back_func(int a,int b, int(*callback)(int , int )){
+  a=a^b;
+  b=a^b;
+  a=a^b;
+  return callback(a,b);
+}
+int main(){
+  int e=10,f=5,sum=0,mul=0;
+  sum = execute_call_back_func(e,f,addition);
+  printf("the answer after the swap and  addition is %d\n",sum);
+  mul = execute_call_back_func(e,f,multiply);
+  printf("the answer after the swap and  multiplication is %d\n",mul);
+  return 0;
+}
