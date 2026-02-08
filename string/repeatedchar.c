96117f0f483bc28b2467c32fca4e404d09eb16d4
Sudhakiran <sudhakirangunda@gmail.com>
2026-02-08 13:57:18 +0530

    Implement repeated character counting in string
    Add functionality to count and display repeated characters in a string.

--- string/repeatedchar.c
+++ string/repeatedchar.c
@@ -1,1 +1,25 @@
+/****count how many repeated letters are there and find out the occurance of those letters*****/
 
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[200],i,j;
+    printf("enter the string\n");
+    scanf("%[^\n]",str);
+    for(i='a';i<='z';i++){
+        int count=0;
+        for(j=0;str[j]!='\0';j++){
+            if (str[j] >= 'A' && str[j] <= 'Z'){
+                        str[j] = str[j] + 32;   // convert to lowercase
+                }
+            if(i==str[j]){
+                count++;
+            }
+        }
+        if(count>=2){
+            printf("%c is repeated %d times\n",i,count);
+        }
+        
+    }
+    return 0;
+}
