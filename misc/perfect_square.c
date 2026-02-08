489fb13c0e0c78ec92f31f38ffa6fc67f484c1f6
Sudhakiran <sudhakirangunda@gmail.com>
2026-02-08 20:09:24 +0530

    Add perfect square checker in perfect_square.c

--- misc/perfect_square.c
+++ misc/perfect_square.c
@@ -1,1 +1,16 @@
-
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int num;
+    printf("enter the number\n");
+    scanf("%d",&num);
+    int root;
+    root = sqrt(num);
+    if((root*root)==num){
+        printf("perfecrt square\n");
+    }
+    else{
+        printf("not a perfect square\n");
+    }
+    return 0;
+}
