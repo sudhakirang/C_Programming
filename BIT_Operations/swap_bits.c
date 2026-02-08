0626b90716e1a76daa507f0491f650e93312130d
Sudhakiran <sudhakirangunda@gmail.com>
2026-02-08 20:18:28 +0530

    Implement bit swapping in swap_bits.c

--- BIT_Operations/swap_bits.c
+++ BIT_Operations/swap_bits.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<math.h>
+int main(){
+    int num;
+    printf("Enter a number: ");
+    scanf("%x", &num);
+    int even_bits = num&0xAAAAAAAA;
+    int odd_bits = num&0x55555555;
+    even_bits = even_bits>>1;
+    odd_bits = odd_bits<<1; 
+    num = even_bits|odd_bits;
+    printf("the number after bit swap is 0x%X\n",num);
+    return 0;
+}
