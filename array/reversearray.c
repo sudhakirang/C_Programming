f767fe31ec5a92f5c853def9606bd9d86bb68768
Sudhakiran <sudhakirangunda@gmail.com>
2026-02-08 20:42:24 +0530

    Implement array reversal in reversearray.c
    This program takes an array of integers as input and reverses its elements.

--- array/reversearray.c
+++ array/reversearray.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    int arr[100], n, i, start, end, temp;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Reverse the array
+    start = 0;
+    end = n - 1;
+    while (start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("Reversed array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d\t", arr[i]);
+    }
+
+    return 0;
+}
