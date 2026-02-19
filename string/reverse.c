e4e1b6c2963daed3dccdba53ab03cd0c7b1541c2
Sudhakiran <sudhakirangunda@gmail.com>
2026-02-19 17:04:30 +0530

    Implement string reversal function
    Added a function to reverse a string and print it.

--- string/reverse.c
+++ string/reverse.c
@@ -19,3 +19,42 @@ int main()
   printf("the string after reverse is %s\n",a);
   return 0;
 }
+
+
+/**another way**/
+#include <stdio.h>
+
+void str_rev(char *src, char *dst)
+{
+    int i, len = 0;
+
+    // Find length
+    for (i = 0; src[i] != '\0'; i++) {
+        len++;
+    }
+
+    // Reverse copy
+    for (i = 0; i < len; i++) {
+        dst[i] = src[len - i - 1];
+    }
+
+    dst[len] = '\0';   // IMPORTANT
+
+    printf("Destination string is: %s\n", dst);
+}
+
+int main()
+{
+    char str[20];
+    char dst[20];
+
+    printf("Enter the string:\n");
+    scanf("%[^\n]", str);
+
+    printf("Entered string: %s\n", str);
+    printf("Destination before reverse: %s\n", dst);
+
+    str_rev(str, dst);
+
+    return 0;
+}
